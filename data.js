4393967c181c70353a490fb1223afb0bad3ba03b
sourav <kumarsourav7012@gmail.com>
2024-08-04 00:11:00 +0530

    learning-node

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name:'sourav',age:'19',city:'ghaziabad'},
+    {name:'lakshita',age:'17',city:'ud'},
+    {name:'aman',age:'16',city:'ghaziabad'}
+]
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// const http = require('http');
+const http = require('http');
+const data = require('./data')
 
-// http.createServer((req, res) => {
-//    res.write('<h1>hi, i am sourav</h1>');
-//    res.end();
-// }).listen(3000);
-
-const colors = require('colors');
-console.log('sourav kumar'.blue);
+http.createServer((req, res) => {
+   res.writeHead(200,{'Content-Type':'application\json'});
+   res.write(JSON.stringify(data));
+   res.end();
+}).listen(3000);
