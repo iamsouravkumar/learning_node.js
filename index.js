0c6959852e29c2493caa7795fc00cea914cee04c
sourav <kumarsourav7012@gmail.com>
2024-08-03 20:40:34 +0530

    learning_node

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req,res)=>{
-   res.write('hi, i am sourav')
-   res.end()
-}).listen(3000)
+// http.createServer((req, res) => {
+//    res.write('<h1>hi, i am sourav</h1>');
+//    res.end();
+// }).listen(3000);
+
+const colors = require('colors');
+console.log('sourav kumar'.blue);
