eea456de1bedef77a58c4fd72ccf408ed89d25c8
sourav <kumarsourav7012@gmail.com>
2024-08-04 17:20:40 +0530

    learning-node

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'crud');
+const filePath = `${dirPath}/crud.txt`
 
-const input = process.argv
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(dirPath+`/hello${i}.txt`,`hello world${i}`)
+// }
 
-if (input[2] == 'add') {
-    fs.writeFileSync(input[3], input[4]);
-}
-else if (input[2] == 'remove') {
-    fs.unlinkSync(input[3])
-}
-else {
-    console.log('invalid input');
-}
+// fs.writeFileSync(filePath,'this is our first crud file')
+
+// fs.readFile(filePath,'utf8',(err,item)=>{
+//     console.log(item);
+// })
+
+// fs.appendFile(filePath, ' created with node.js', (err) => {
+//     if (!err) console.log('file updated successfully')
+// })
+
+// fs.rename(filePath,`${dirPath}/CRUD.txt`,(err)=>{
+//     if(!err) console.log('file rename successfully');
+// })
+
+fs.unlinkSync(`${dirPath}/CRUD.txt`);
